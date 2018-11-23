536161c7528712acf9ddac1afab606aa1a4b5611
Dinçer Ünal <dincer.unal0652@gmail.com>
2018-11-23 10:58:20 +0300

    Initial commit

---
+++ commandprompt/SiemensCommandPrompt.h
@@ -0,0 +1,31 @@
+
+
+/**
+ * @author karans
+ * @version 1.0
+ * @created 05-Haz-2017 15:42:54
+ */
+public class SiemensCommandPrompt {
+
+	private string m_prompt;
+	public CmdInfo m_CmdInfo;
+
+	public SiemensCommandPrompt(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	/**
+	 * 
+	 * @param prompt
+	 */
+	public SiemensCommandPrompt(string prompt){
+
+	}
+
+	public void run(){
+
+	}
+}//end SiemensCommandPrompt
